3f9d45f729b0762e784d542c411492434bfd0333
JunhoCho <46771005+aimho23@users.noreply.github.com>
2020-10-31 14:49:08 +0900

    Add files via upload

--- 기본이미지분류.py
+++ 기본이미지분류.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 import tensorflow as tf
@@ -36,27 +36,84 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover','Dress','Coat','Sandal','Shi
 train_images.shape
 
 
-# In[8]:
+# In[7]:
 
 
 len(train_labels)
 
 
-# In[10]:
+# In[8]:
 
 
 train_labels
 
 test_images.shape
-# In[12]:
+# In[9]:
 
 
 test_images.shape
 len(test_labels)
 
 
-# In[ ]:
+# In[14]:
 
 
 #전처리 진행해야함
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+
+# In[15]:
+
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+
+# In[17]:
+
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
+
+
+# In[18]:
+
+
+#모델구성해야함
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128, activation = 'relu'),
+    keras.layers.Dense(10, activation = 'softmax')
+])
+
+
+# In[20]:
+
+
+model.compile(optimizer='adam',
+             loss='sparse_categorical_crossentropy',
+             metrics=['accuracy'])
+
+
+# In[21]:
+
+
+#모델훈련해야함
+
+
+# In[ ]:
+
+
+
 
